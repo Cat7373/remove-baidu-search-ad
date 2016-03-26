9543cf4d87f22b1229e5788e014dc21b6bebd3c1
Cat73 <1901803382@qq.com>
2016-03-27 01:02:47 +0800

    修复意外移除百度百科及新闻的问题

--- remove_baidu_search_ad.js
+++ remove_baidu_search_ad.js
@@ -15,13 +15,13 @@
 // @supportURL        https://github.com/Cat7373/remove-baidu-search-ad/issues/
 // @updateURL         https://raw.githubusercontent.com/Cat7373/remove-baidu-search-ad/master/remove_baidu_search_ad.js
 
-// @compatible        chrome 49.0.2623.75 + TamperMonkey + 脚本_0.1 测试通过
+// @compatible        chrome 49.0.2623.75 + TamperMonkey + 脚本_0.1.2 测试通过
 // @compatible        firefox 未测试
 // @compatible        opera 未测试
 // @compatible        safari 未测试
 
 // @author            Cat73
-// @version           0.1.1
+// @version           0.1.2
 // @license           LGPLv3
 
 // @match             http://*.baidu.com/*
@@ -34,13 +34,13 @@
 function clearLoop() {
     var result_div = document.getElementById("content_left");
     if(result_div) {
-        var results = result_div.getElementsByTagName('div');
-        for(var id in results) {
-            var current_div = results[id];
-            if(current_div.parentNode == result_div) {
-                if(current_div.className != "result c-container ") {
-                    result_div.removeChild(current_div);
-                    console.debug("Done!");
+        var nodes = result_div.childNodes;
+        for(var id in nodes) {
+            var current = nodes[id];
+            if(current.nodeName == "DIV") {
+                if(current.className.indexOf("result") == -1) {
+                    current.innerHTML = "";
+                    current.style.display = "none";
                     break;
                 }
             }
